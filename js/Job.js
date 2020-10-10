fcfde97161ee694336c930a8fe5525294af47a53
Administrator <admin@example.com>
2020-10-10 19:52:33 +0100

    data structure categories and jobs

---
+++ js/Job.js
@@ -0,0 +1,24 @@
+class Job {
+    
+  constructor(title, compagnie, description, validated_at, category, city) {
+    this.title = title;
+    this.companie = compagnie;
+    this.description = description;
+    this.validated_at = validated_at;
+    this.city = city;
+    this.created_at = this.dateNow();
+  }
+
+  dateNow() {
+    return new Date().toISOString().slice(0, 10);
+  }
+}
+
+let job1 = new Job(
+  "Dev Symfony 4",
+  "HIT Consution",
+  "senoir dev apps with cdi",
+  "2020-10-09",
+  "4",
+  "casablanca"
+);
---
+++ js/data.js
@@ -0,0 +1,229 @@
+export const jobs = [
+  {
+    id: "1",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 1
+  },
+  {
+    id: "2",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 1
+  },
+  {
+    id: "3",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 1
+  },
+  {
+    id: "4",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 2
+  },
+  {
+    id: "5",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 1
+  },
+  {
+    id: "6",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 2
+  },
+  {
+    id: "7",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 2
+  },
+  {
+    id: "8",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 2
+  },
+  {
+    id: "9",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 3
+  },
+  {
+    id: "10",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 3
+  },
+  {
+    id: "11",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 4
+  },
+  {
+    id: "12",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "13",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "14",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "15",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "16",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "17",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 5
+  },
+  {
+    id: "18",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 6
+  },
+  {
+    id: "19",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 7
+  },
+  {
+    id: "20",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 6
+  },
+
+  {
+    id: "21",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 6
+  },
+  {
+    id: "22",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 7
+  },
+  {
+    id: "23",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 4
+  },
+  {
+    id: "25",
+    title: "",
+    compagnie: "",
+    description: "",
+    validated_at: "",
+    created_at: "",
+    category: 7
+  }
+];
+
+export const categories = [
+  { id: "1", name: "Achat" },
+  { id: "2", name: "Métier du BTP" },
+  { id: "3", name: "Production,maintenance, qualité" },
+  { id: "4", name: "RH, formation" },
+  { id: "5", name: "Sécrétariat, assistanat" },
+  { id: "6", name: "Transport, logistique" },
+  { id: "7", name: "Informatique" }
+];
